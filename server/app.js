5dbfcea804abee9b4c2b666fe2a6a93b777c9229
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-03 21:39:24 -0500

    Connect to Mongo Database

--- server/app.js
+++ server/app.js
@@ -11,3 +11,9 @@ const router = require('./router.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+mongoose.connect(dbURI).catch((err) => {
+    if(err) {
+        console.log('Could not connect to database.');
+        throw err;
+    }
+});
