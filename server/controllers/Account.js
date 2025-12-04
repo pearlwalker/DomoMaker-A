15020d9a53d03bd0630aba8e27727d30d6749e27
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 14:50:11 -0500

    Reformatted logout const

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,7 +6,9 @@ const loginPage = (req, res) => res.render('login');
 
 const signupPage = (req, res) => res.render('signup');
 
-const logout = (req, res) => res.redirect('/');
+const logout = (req, res) => { 
+  res.redirect('/'); 
+};
 
 const login = (req, res) => {
   const username = `${req.body.username}`;
