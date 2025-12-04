b07b853c2fdbc51b4d555a8b0e5bb359be92b3c9
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 16:32:58 -0500

    Redirect user to app if they're already logged in

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -6,6 +6,9 @@ const requiresLogin = (req, res, next) => {
 };
 
 const requiresLogout = (req, res, next) => {
+    if(req.session.account) {
+        return res.redirect('/maker');
+    }
     return next();
 };
 
