d6c411a8f262f121d89ca18f300a7446012911c6
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-03 21:35:33 -0500

    Require mongoose

--- server/app.js
+++ server/app.js
@@ -2,3 +2,4 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
