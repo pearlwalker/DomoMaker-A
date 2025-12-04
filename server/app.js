014aa921d77353951e74e2fc29c08be7acda46a1
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 16:24:33 -0500

    ESLint Formatting

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const session = require('express-session');
-const RedisStore = require('connect-redis').RedisStore;
+const { RedisStore } = require('connect-redis');
 const redis = require('redis');
 
 const router = require('./router.js');
