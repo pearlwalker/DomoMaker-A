c60c2b4493a36e0ca36e4d77eb1844b2d50d7c56
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 16:39:36 -0500

    Require secure connection and require logout for /signup and /login

--- server/router.js
+++ server/router.js
@@ -2,11 +2,11 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/login', controllers.Account.loginPage);
-  app.post('/login', controllers.Account.login);
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
-  app.get('/signup', controllers.Account.signupPage);
-  app.post('/signup', controllers.Account.signup);
+  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
   app.get('/logout', controllers.Account.logout);
 
