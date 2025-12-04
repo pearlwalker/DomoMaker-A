b76868a4c320fb9133bb6041a5cd07165f27f03d
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 15:10:08 -0500

    Added badRequest missing parameter handling to makeDomo

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -7,7 +7,9 @@ const makerPage = (req, res) => {
 };
 
 const makeDomo = async (req, res) => {
-
+  if(!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'Both name and age are required!' });
+  }
 };
 
 module.exports = {
