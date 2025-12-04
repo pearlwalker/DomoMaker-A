e1b1fbc87cf5a20e81a5c8945caf485c162a6b8b
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 16:34:35 -0500

    Check if user's connection is secure, redirect if not

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -13,6 +13,9 @@ const requiresLogout = (req, res, next) => {
 };
 
 const requiresSecure = (req, res, next) => {
+    if(req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(`https://${req.hostname}${req.url}`);
+    }
     return next();
 };
 
