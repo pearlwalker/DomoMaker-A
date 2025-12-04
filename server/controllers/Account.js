17e5b163a86c342b3157abfe457fbcc9fb112da6
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 14:28:48 -0500

    Added return statement to the end of try

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -29,6 +29,7 @@ const signup = async (req, res) => {
     const hash = await Account.generateHash(pass);
     const newAccount = new Account({ username, password: hash });
     await newAccount.save();
+    return res.json({ redirect: '/maker'});
   } catch (err) {
     console.log(err);
     if(err.code === 11000) {
