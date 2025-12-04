ecf97503c68959334be39b4cd2beeb6818575cc5
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 16:31:15 -0500

    Functions end in calls to next function

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,17 +1,17 @@
 const requiresLogin = (req, res, next) => {
-
+    return next();
 };
 
 const requiresLogout = (req, res, next) => {
-
+    return next();
 };
 
 const requiresSecure = (req, res, next) => {
-
+    return next();
 };
 
 const bypassSecure = (req, res, next) => {
-
+    next();
 };
 
 module.exports.requiresLogin = requiresLogin;
