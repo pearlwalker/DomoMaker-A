07837f5878b9b5e7cfae6b61a51682814f12b040
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-12-04 14:25:38 -0500

    Added additional conditional (rhyme) in catch block

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -29,6 +29,9 @@ const signup = async (req, res) => {
 
   } catch (err) {
     console.log(err);
+    if(err.code === 11000) {
+        return res.status(400).json({ error: 'Username already in use!' });
+    }
   }
 };
 
